4fef5802dfad896bd5b9c0862e76c708bc088b78
Youyuan Lu <yol093@ucsd.edu>
2016-11-05 22:29:10 +0000

    warm up completed

--- reverse_endian.c
+++ reverse_endian.c
@@ -3,5 +3,26 @@
 #include "warmup.h"
 
 int main(int argc, char** argv){
-     /* Your main code for reversing endian-ness goes here*/
+  /* reverse the endianness of each input integer and output them */
+  FILE * in;
+  FILE * out;
+  int num;
+
+  if (argc != 3)  // incorrect number of arguments
+    return 0;
+
+  in = fopen(argv[1], "rb");
+  out = fopen(argv[2], "wb");
+  if (in == NULL)  // invalid argument
+    return 0;
+
+  // read input integers, reorder each of them, and output them
+  while (fread(&num, sizeof(int), 1, in) == 1) {
+    num = reorder_bytes(num);
+    fwrite(&num, sizeof(int), 1, out);
+  }
+
+  fclose(in);
+  fclose(out);
+  return 0;
 }
--- warmup.c
+++ warmup.c
@@ -3,10 +3,28 @@
 #include "warmup.h"
 
 unsigned int *char_freqs(FILE *fp){
-   /* Your code goes here*/
+  /* get the frequence count of each character in the file */
+  unsigned int * arr = calloc(256, sizeof(unsigned int));
+  int ch;
+
+  if (fp == NULL)  // return null when fp is null
+    return NULL;
+
+  while ((ch = fgetc(fp)) != EOF)  // traverse the file to count frequence
+    arr[ch]++;
+
+  return arr;
 }
-  
+
 
 int reorder_bytes(int num){
-   /* Your code goes here*/
+  /* reverse the endianness of the number */
+  int byte0 = (num & 0xFF) << 24;  // get 4 bytes of the number
+  int byte1 = (num & 0xFF00) << 8;
+  int byte2 = (num >> 8) & 0xFF00;
+  int byte3 = (num >> 24) & 0xFF;
+
+  int val = byte0 + byte1 + byte2 + byte3;  // reverse the order of bytes
+
+  return val;
 }
